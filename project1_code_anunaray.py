d2786028b69b3c345dc39a5798b7e524415ae476
Anu Narayan <anunarayan@Anus-MacBook-Pro.local>
2025-10-10 13:38:02 -0400

    Imported CSV file and created main code file

---
+++ project1_code_anunaray.py
@@ -0,0 +1,16 @@
+import csv
+
+def load_penguin_stats(fname = 'penguins.csv'):
+    pass
+
+def isolate_species():
+    pass
+
+def find_penguin_distribution(penguin):
+    pass
+
+def sort_species():
+    pass
+
+def main():
+    pass
